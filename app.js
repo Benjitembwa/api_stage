97ba2e46c405eb6503b10bdf87f70503d4540da5
Benjitembwa <benjitembwa7@gmail.com>
2025-04-21 11:59:31 +0100

    fin

--- app.js
+++ app.js
@@ -21,12 +21,7 @@ connectDB();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/etudiants", etudiantRoutes);
 app.use("/api/mentions", mentionRoutes);
